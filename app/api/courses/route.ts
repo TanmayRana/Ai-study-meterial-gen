ff969bbdd80814a0118cc7864da26412516bbbec
Tanmay Rana <tanmayrana2001@gmail.com>
2025-05-07 19:01:45 +0530

    push all code 9th

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -21,16 +21,46 @@ export async function POST(req: Request) {
   }
 }
 
+// export async function GET(req: Request) {
+//   try {
+//     const reqUrl = req.url;
+//     const { searchParams } = new URL(reqUrl);
+//     const courseId = searchParams.get("courseId");
+
+//     const course = await db
+//       .select()
+//       .from(STUDY_MATERIAL_TABLE)
+//       .where(eq(STUDY_MATERIAL_TABLE?.courseId, courseId));
+
+//     return Response.json({
+//       result: course[0],
+//       message: "Course fetched successfully.",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return Response.json({
+//       message: "Something went wrong while fetching course.",
+//     });
+//   }
+// }
+
 export async function GET(req: Request) {
   try {
     const reqUrl = req.url;
     const { searchParams } = new URL(reqUrl);
     const courseId = searchParams.get("courseId");
 
+    if (!courseId) {
+      return Response.json(
+        { message: "Missing courseId in query." },
+        { status: 400 }
+      );
+    }
+
     const course = await db
       .select()
       .from(STUDY_MATERIAL_TABLE)
-      .where(eq(STUDY_MATERIAL_TABLE?.courseId, courseId));
+      .where(eq(STUDY_MATERIAL_TABLE.courseId, courseId));
 
     return Response.json({
       result: course[0],
@@ -38,8 +68,9 @@ export async function GET(req: Request) {
     });
   } catch (error) {
     console.log(error);
-    return Response.json({
-      message: "Something went wrong while fetching course.",
-    });
+    return Response.json(
+      { message: "Something went wrong while fetching course." },
+      { status: 500 }
+    );
   }
 }
