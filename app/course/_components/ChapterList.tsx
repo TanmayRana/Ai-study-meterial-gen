55c91d30239ae76ab17f9bdc7aaef7fd30bcc98c
Tanmay Rana <tanmayrana2001@gmail.com>
2025-05-07 19:16:51 +0530

    push all code 10th

--- app/course/_components/ChapterList.tsx
+++ app/course/_components/ChapterList.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 
-const ChapterList = ({ course }) => {
+const ChapterList = ({ course }: any) => {
   const Chapters = course?.courseLayout?.chapters || []; // Default to empty array if Chapters is undefined
 
   console.log("Chapters =", Chapters);
@@ -9,7 +10,7 @@ const ChapterList = ({ course }) => {
     <div className="mt-5">
       <h2 className="font-medium text-xl">Chapters</h2>
       <div className="mt-3">
-        {Chapters.map((chapter, index) => (
+        {Chapters.map((chapter: any, index: any) => (
           <div
             className="flex gap-5 items-center p-4 mb-2 border shadow-md rounded-lg cursor-pointer"
             key={index}
